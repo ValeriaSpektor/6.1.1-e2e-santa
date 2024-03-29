951fbb3eef0b73a7097e39306a4c09e94e0a2c52
Valeria Spektor <vlapin84@gmail.com>
2024-03-29 21:22:39 +0300

    removed file

--- cypress/e2e/santa-deleteBox.cy.js
+++
@@ -1,7 +0,0 @@
-describe("user can delete box using API", () => {
-    it("delete box", () => {
-      // Здесь можно вызвать API для удаления коробки
-      cy.deleteBox("your_box_id");
-    });
-  });
-  
