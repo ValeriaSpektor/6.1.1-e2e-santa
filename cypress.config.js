cdea79f75121776c12f4633c0458853816722adb
Valeria Spektor <vlapin84@gmail.com>
2024-04-05 20:29:37 +0300

    added id project for 7.1.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '1tgoib',
   e2e: {
     baseUrl: "https://staging.lpitko.ru",
     testIsolation: false,
