692bcae273f6c362f7f5d2f2ba998b1a8d7da2e4
Valeria Spektor <vlapin84@gmail.com>
2024-04-17 15:20:43 +0300

    updated config for 11.1.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,23 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  projectId: '1tgoib',
   e2e: {
     baseUrl: "https://staging.lpitko.ru",
     testIsolation: false,
+    specPattern: "cypress/e2e/hw_7.1.1/box-test.js", // Modified spec pattern
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+      on("file:preprocessor", bundler);
+      addCucumberPreprocessorPlugin(on, config);
+      allureWriter(on, config);
+      return config;
     },
+    pageLoadTimeout: 30000, 
   },
 });
